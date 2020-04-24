2127c66d7663aee481ead25bfa3c022332bb1251
Matias Schroder <matischroder99@gmail.com>
2020-04-24 14:15:44 -0300

    se progreso en el formulario

--- src/Global/Form.jsx
+++ src/Global/Form.jsx
@@ -1,32 +1,66 @@
-import React, {Component} from "react";
-
-class Form extends Component{
-    render(){
-        return(
-            <div>
-                <h2>Donen ratas</h2>
-                <p>
-                    Esta es la pagina web para realizar donaciones en el voluntarioado de
-                    ingenieria de la Universidad Catolica de Cordoba. <br />
-                    Su propuesta va a ser analizada y luego uno de nuestros representantes
-                    va a responderle si fue aprobada o no.
-                </p>
-                <form>
-                    <a>Nombre</a>
-                    <input placeholder="Nombre"/>
-                    <a>Apellido</a>
-                    <input placeholder="Apellido"/>
-                    <input placeholder="Direccion"/>
-                    <input placeholder="Telefono"/>
-                    <input placeholder="Algo mas que quieras poner perreq"/>
-                    <input placeholder="Tipo de donacion a"/>
-                    <input placeholder="Cantidad ax2"/>
-                    <input placeholder="Como se entero opciones"/>
-                </form>
-                <a href="soyunbotonperrek">Confirmar</a>
-            </div>
-        );
-    }
+import React, { Component } from "react";
+import "./css/Form.css";
+class Form extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Donen</h2>
+        <p>
+          Esta es la pagina web para realizar donaciones en el voluntarioado de
+          ingenieria de la Universidad Catolica de Cordoba. <br />
+          Su propuesta va a ser analizada y luego uno de nuestros representantes
+          va a responderle si fue aprobada o no.
+        </p>
+        <form id="formulario">
+          <h5>Donacion</h5>
+          <ul>
+            <li id="nombreCompleto">
+              <a className="acincuenta" for="name">
+                Nombre
+              </a>
+              <a className="acincuenta">Apellido</a>
+              <br />
+              <input type="text"></input>
+              <input type="text"></input>
+            </li>
+            <li>
+              <a className="titles">Teléfono</a>
+              <input
+                id="telefono"
+                type="tel"
+                placeholder="Teléfono 351 XXX XXXX"
+              ></input>
+            </li>
+            <li>
+              <a className="titles">Mail</a>
+              <input type="email" placeholder="e-Mail"></input>
+            </li>
+            <li>
+              <a className="titles">Su consulta</a>
+              <textarea for="msg" id="consulta"></textarea>
+            </li>
+            <li>
+              <input placeholder="Direccion" />
+            </li>
+            <li>
+              <input placeholder="Tipo de donacion a" />
+            </li>
+            <li>
+              {" "}
+              <input placeholder="Cantidad ax2" />
+            </li>
+            <li>
+              {" "}
+              <input placeholder="Como se entero opciones" />
+            </li>
+          </ul>
+          <a id="enviar" href="">
+            Enviar
+          </a>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Form;
