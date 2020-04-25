0d5d4caf3740630cd754061ef860291a4b518d07
Matias Schroder <matischroder99@gmail.com>
2020-04-25 13:25:06 -0300

    Cambios de elemenros requeridos para mandar el formulario, cambio diseno de direccion

--- src/Global/Form.jsx
+++ src/Global/Form.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./css/Form.css";
+import Conteiner from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -8,43 +9,53 @@ import Row from "react-bootstrap/Row";
 class Formulario extends Component {
   render() {
     return (
-      <div id="web">
-                  <h2>Donen</h2>
-                  <p>
-                      Esta es la pagina web para realizar donaciones en el voluntariado de
-                      ingenieria de la Universidad Catolica de Cordoba. <br />
-                      Su propuesta va a ser analizada y luego uno de nuestros representantes
-                      va a responderle si fue aprobada o no.
-                  </p>
-        <Form id="formulario">
-        <Form.Group controlId="name">
-          <Form.Label>Nombre</Form.Label>
-          <Form.Control type="text" placeholder="Apellido" />
-        </Form.Group>
+      <Conteiner id="formulario">
+        <h2>Formulario</h2>
+        <p>
+          Esta es la pagina web para realizar donaciones en el voluntariado de
+          ingenieria de la Universidad Catolica de Cordoba. <br />
+          Su propuesta va a ser analizada y luego uno de nuestros representantes
+          va a responderle si fue aprobada o no.
+        </p>
+        <Form>
+          <Form.Group controlId="name">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control type="text" placeholder="Nombre" required />
+          </Form.Group>
 
-        <Form.Group controlId="lastName">
-          <Form.Label>Apellido</Form.Label>
-          <Form.Control type="text" placeholder="Apellido" />
-        </Form.Group>
+          <Form.Group controlId="lastName">
+            <Form.Label>Apellido</Form.Label>
+            <Form.Control type="text" placeholder="Apellido" required />
+          </Form.Group>
 
-        <Form.Group controlId="tel">
-          <Form.Label>Teléfono</Form.Label>
-          <Form.Control type="tel" placeholder="351 XXX XXXX" />
-        </Form.Group>
+          <Form.Group controlId="tel">
+            <Form.Label>Teléfono</Form.Label>
+            <Form.Control type="tel" placeholder="351 000 0000" required />
+          </Form.Group>
 
           <Form.Group controlId="formEmail">
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="example@mail.com" />
+            <Form.Control
+              type="email"
+              placeholder="example@mail.com"
+              required
+            />
           </Form.Group>
-
-        <Form.Group controlId="dire">
           <Form.Label>Dirección</Form.Label>
-          <Form.Control type="text" placeholder="Ciudad, calle, altura" />
-        </Form.Group>
-
+          <Form.Row>
+            <Form.Group as={Col} md="5" controlId="ciudad">
+              <Form.Control type="text" placeholder="Ciudad" required />
+            </Form.Group>
+            <Form.Group as={Col} md="5" controlId="calle">
+              <Form.Control type="text" placeholder="Calle" required />
+            </Form.Group>
+            <Form.Group as={Col} md="2" controlId="altura">
+              <Form.Control type="number" placeholder="Altura" required />
+            </Form.Group>
+          </Form.Row>
           <Form.Group controlId="exampleForm.ControlSelect2">
             <Form.Label>Tipo de donación</Form.Label>
-            <Form.Control as="select" multiple>
+            <Form.Control as="select" multiple required>
               <option>Material de construccion</option>
               <option>Elementos de banio</option>
               <option>Herramientas</option>
@@ -60,37 +71,40 @@ class Formulario extends Component {
               </Form.Label>
               <Col sm={10}>
                 <Form.Check
-                    type="radio"
-                    label="first radio"
-                    name="formHorizontalRadios"
-                    id="formHorizontalRadios1"
+                  type="radio"
+                  label="first radio"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios1"
                 />
                 <Form.Check
-                    type="radio"
-                    label="second radio"
-                    name="formHorizontalRadios"
-                    id="formHorizontalRadios2"
+                  type="radio"
+                  label="second radio"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios2"
                 />
                 <Form.Check
-                    type="radio"
-                    label="third radio"
-                    name="formHorizontalRadios"
-                    id="formHorizontalRadios3"
+                  type="radio"
+                  label="third radio"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios3"
                 />
               </Col>
             </Form.Group>
           </fieldset>
 
           <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Si prefiere describir una cantidad no convencional, describalo aqui</Form.Label>
+            <Form.Label>
+              Si prefiere describir una cantidad no convencional, describalo
+              aqui
+            </Form.Label>
             <Form.Control as="textarea" rows="3" />
           </Form.Group>
 
-        <Button id="enviar" type="submit">
-          Enviar
-        </Button>
-      </Form>
-      </div>
+          <Button type="submit" id="enviar">
+            Enviar
+          </Button>
+        </Form>
+      </Conteiner>
     );
   }
 }
