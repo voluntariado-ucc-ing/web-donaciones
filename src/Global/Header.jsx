85fa1b7b338529ce9804eb300642d714cf37649b
MilenaL2018 <milelucero_09@hotmail.com>
2020-04-27 18:36:34 -0300

    Nav justified to end

--- src/Global/Header.jsx
+++ src/Global/Header.jsx
@@ -7,8 +7,8 @@ class Header extends Component{
     render(){
         return(
            <div>
-               <Nav variant="pills" defaultActiveKey="/">
-               <Navbar bg="light" >
+               <Nav variant="pills" defaultActiveKey="/" className="justify-content-end">
+               <Navbar bg="light">
                    <Navbar.Brand href="/home">
                        <img
                            alt="ucc"
@@ -18,12 +18,10 @@ class Header extends Component{
                            className="d-inline-block align-top"
                        />
                    </Navbar.Brand>
-                   <Nav className="mr-auto">
                        <Nav.Link href="/home">Inicio</Nav.Link>
                        <Nav.Link eventKey="link-1" href="/form">Donaciones</Nav.Link>
-                   </Nav>
                </Navbar>
-               </Nav>
+             </Nav>
            </div>
         );
     }
