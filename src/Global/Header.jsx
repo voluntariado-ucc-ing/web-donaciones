9d34f7bda802af90307fd88d5de4e7447a2ff830
CarolinaMisa <1700250@ucc.edu.ar>
2020-05-01 19:29:17 -0300

    Header and descriptions - text and background - changed

--- src/Global/Header.jsx
+++ src/Global/Header.jsx
@@ -2,26 +2,27 @@ import React, {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import "././css/Header.css";
 
 class Header extends Component{
     render(){
         return(
-           <div>
-               <Nav variant="pills" defaultActiveKey="/" className="justify-content-end">
-               <Navbar bg="light">
-                   <Navbar.Brand href="/home">
-                       <img
-                           alt="ucc"
-                           src={"https://www.uccor.edu.ar/portal2015/UniversidadCatolica/images/logo-ucc-2018.svg"}
-                           width="80"
-                           height="40"
-                           className="d-inline-block align-top"
-                       />
-                   </Navbar.Brand>
-                       <Nav.Link href="/home">Inicio</Nav.Link>
-                       <Nav.Link eventKey="link-1" href="/form">Donaciones</Nav.Link>
-               </Navbar>
-             </Nav>
+           <div><Navbar variant="pills" className="navbar">
+               <Navbar.Brand>
+                   <img
+                       alt="ucc"
+                       src={"https://www.uccor.edu.ar/portal2015/UniversidadCatolica/images/logo-ucc-2018.svg"}
+                       width="80"
+                       height="40"
+                       className="d-inline-block align-top"
+                   />
+               </Navbar.Brand>
+               <Nav className="ml-auto">
+                   <Nav.Link href="/home" className="b1">Inicio</Nav.Link>
+                   <Nav.Link eventKey="link-1" className= "b2" href="/form">Donaciones</Nav.Link>
+               </Nav>
+           </Navbar>
+               
            </div>
         );
     }
