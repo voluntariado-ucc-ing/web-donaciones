e313e83039a35849f0c8cb4c2479aa68a57b0926
Matias Schroder <matischroder99@gmail.com>
2020-05-07 20:34:13 -0300

    Se agrego el scroll en el navbar

--- src/Global/Header.jsx
+++ src/Global/Header.jsx
@@ -1,29 +1,80 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "././css/Header.css";
+import { Link } from "react-scroll";
 
-class Header extends Component{
-    render(){
-        return(
-           <div><Navbar variant="pills" className="navbar">
-               <Navbar.Brand>
-                   <img
-                       alt="ucc"
-                       src={"https://www.uccor.edu.ar/portal2015/UniversidadCatolica/images/logo-ucc-2018.svg"}
-                       width="80"
-                       height="40"
-                       className="d-inline-block align-top"
-                   />
-               </Navbar.Brand>
-               <Nav className="ml-auto">
-                   <Nav.Link href="/" className="b1">Inicio</Nav.Link>
-                   <Nav.Link eventKey="link-1" className= "b2" href="/form">Donaciones</Nav.Link>
-               </Nav>
-           </Navbar>
-               
-           </div>
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar variant="pills" className="navbar" fixed="top">
+                    <Navbar.Brand>
+                        <img
+                            alt="ucc"
+                            src={"https://www.uccor.edu.ar/portal2015/UniversidadCatolica/images/logo-ucc-2018.svg"}
+                            width="80"
+                            height="40"
+                            className="d-inline-block align-top"
+                        />
+                    </Navbar.Brand>
+                    <Nav className="ml-auto">
+                        <Link activeClass="active"
+                            to="intro"
+                            spy={true}
+                            smooth={true}
+                            offset={-64}
+                            duration={700}
+                            delay={50}
+                            href="/"
+                            className="b2"
+                        >Inicio</Link>
+                        <Link
+                            to="quienesSomos"
+                            spy={true}
+                            smooth={true}
+                            offset={-64}
+                            duration={700}
+                            delay={50}
+                            href="/"
+                            className="b2"
+                        >Quiénes Somos</Link>
+                        <Link
+                            to="ayudanos"
+                            spy={true}
+                            smooth={true}
+                            offset={-64}
+                            duration={700}
+                            delay={50}
+                            href="/"
+                            className="b2"
+                        >Ayudanos a Ayudar</Link>
+                        <Link
+                            to="queHacemos"
+                            spy={true}
+                            smooth={true}
+                            offset={-64}
+                            duration={700}
+                            delay={50}
+                            href="/"
+                            className="b2"
+                        >Qué hacemos</Link>
+                        <Link
+                            to="dondeEstamos"
+                            spy={true}
+                            smooth={true}
+                            offset={-64}
+                            duration={700}
+                            delay={50}
+                            href="/"
+                            className="b2"
+                        >En dónde</Link>
+                    </Nav>
+                </Navbar>
+
+            </div>
         );
     }
 }
