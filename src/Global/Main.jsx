9c4bd0b7b8c4ae7dbeb2f987e9581c9893270d20
Matias Schroder <matischroder99@gmail.com>
2020-05-07 20:35:53 -0300

    cambio del from anterior al actual

--- src/Global/Main.jsx
+++ src/Global/Main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Home";
-import Form from "./Formcopy";
+import Formulario from "./Formcopy";
 
 
 const Main = () => (
     <div>
         <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/form" component={Form} />
+            <Route path="/form" component={Formulario} />
         </Switch>
     </div>
 );
