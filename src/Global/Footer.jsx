0ce088fb0589c171fc8ca6afd2f7a115b7f8df8e
MilenaL2018 <milelucero_09@hotmail.com>
2020-05-10 16:44:06 -0300

    Footer done

--- src/Global/Footer.jsx
+++ src/Global/Footer.jsx
@@ -5,10 +5,8 @@ import Row from "react-bootstrap/Row";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelopeOpen, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import Col from "react-bootstrap/Col";
 
-const email = <FontAwesomeIcon icon={faEnvelopeOpen} />;
-const phone = <FontAwesomeIcon icon={faPhone}/>;
-const ig = <FontAwesomeIcon icon={faInstagram}/>;
 
 class Footer extends Component {
     render() {
@@ -19,9 +17,18 @@ class Footer extends Component {
                         <p>Voluntariado Barrio El Milagro</p>
                     </Row>
                     <Row className={"connect"}>
-                       <p> voluntariado.vmru@ucc.edu.ar</p>
-                        <p>@voluntariadouccing</p>
-                        <p>(0351) 493 8000 int. 510</p>
+                        <Col>
+                            <FontAwesomeIcon icon={faEnvelopeOpen}/>
+                            <p>voluntariado.vmru@ucc.edu.ar</p>
+                        </Col>
+                        <Col>
+                            <FontAwesomeIcon icon={faInstagram}/>
+                            <p>@voluntariadouccing</p>
+                        </Col>
+                        <Col>
+                            <FontAwesomeIcon icon={faPhone}/>
+                            <p>(0351) 493 8000 int. 510</p>
+                        </Col>
                     </Row>
                 </Container>
             </div>
