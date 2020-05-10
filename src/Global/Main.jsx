5518a13896c93792eb73c526e7c7904ca787bc74
MilenaL2018 <milelucero_09@hotmail.com>
2020-05-10 11:28:42 -0300

    Revert "Merge branch 'master' of https://github.com/voluntariado-ucc-ing/web-donaciones"
    This reverts commit 6150f13d2532959f521ab4b9ea831be5f0594025, reversing
    changes made to f30c656fb6245848cc01382da8c7c0aa7c650db7.

--- src/Global/Main.jsx
+++ src/Global/Main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Home";
-import Formulario from "./Formcopy";
+import Form from "./Formcopy";
 
 
 const Main = () => (
     <div>
         <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/form" component={Formulario} />
+            <Route path="/form" component={Form} />
         </Switch>
     </div>
 );
