cc6c43f4e41341b6b13e9774664ce1dfe9749cc6
MilenaL2018 <milelucero_09@hotmail.com>
2020-05-10 17:40:47 -0300

    Mobile menu, logo need to be on the left corner

--- src/Global/Header.jsx
+++ src/Global/Header.jsx
@@ -4,7 +4,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "././css/Header.css";
 import { Link } from "react-scroll";
-import iconoCatolica from "../images/iconoCatolica.svg"
+import iconoCatolica from "../images/iconoCatolica.svg";
 
 class Header extends Component {
     render() {
@@ -74,6 +74,81 @@ class Header extends Component {
                     </Nav>
                 </Navbar>
 
+                <Nav defaultActiveKey="/home" className="flex-column">
+                <Navbar className={"mobile-menu"}>
+                    <Navbar.Brand>
+                        <img
+                            alt="ucc"
+                            src={iconoCatolica}
+                            width="80"
+                            height="40"
+                            className="d-inline-block align-top"
+                        />
+                    </Navbar.Brand>
+                    <ul>
+                        <li>
+                        <Link activeClass="active"
+                              to="intro"
+                              spy={true}
+                              smooth={true}
+                              offset={-64}
+                              duration={700}
+                              delay={50}
+                              href="/"
+                              className="b2"
+                        >Inicio</Link>
+                        </li>
+                        <li>
+                        <Link
+                            to="quienesSomos"
+                            spy={true}
+                            smooth={true}
+                            offset={-64}
+                            duration={700}
+                            delay={50}
+                            href="/"
+                            className="b2"
+                        >Quiénes Somos</Link>
+                        </li>
+                        <li>
+                        <Link
+                            to="ayudanos"
+                            spy={true}
+                            smooth={true}
+                            offset={-64}
+                            duration={700}
+                            delay={50}
+                            href="/"
+                            className="b2"
+                        >Ayudanos a Ayudar</Link>
+                        </li>
+                        <li>
+                        <Link
+                            to="queHacemos"
+                            spy={true}
+                            smooth={true}
+                            offset={-64}
+                            duration={700}
+                            delay={50}
+                            href="/"
+                            className="b2"
+                        >Qué hacemos</Link>
+                        </li>
+                        <li>
+                        <Link
+                            to="dondeEstamos"
+                            spy={true}
+                            smooth={true}
+                            offset={-64}
+                            duration={700}
+                            delay={50}
+                            href="/"
+                            className="b2"
+                        >En dónde</Link>
+                        </li>
+                    </ul>
+                </Navbar>
+                </Nav>
             </div>
         );
     }
