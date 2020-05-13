59594e1ee9d549ef046f4383ea8e7f9f9b5320da
Matias Schroder <matischroder@github.com>
2020-05-13 14:37:55 -0300

    Se introdujo la opcion de campo obligatorio al mail

--- src/Global/components/form/0.1Verification.jsx
+++ src/Global/components/form/0.1Verification.jsx
@@ -2,56 +2,68 @@ import React, { Component } from 'react';
 import IntlTelInput from 'react-intl-tel-input';
 import Form from 'react-bootstrap/Form';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import '../../css/Formcopy.css';
 
 class Verification extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            wrongEmail: false
+        }
+    }
+
     back = (e) => {
         e.preventDefault()
         this.props.prevStep()
     }
     continue = (e) => {
+        var emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
         e.preventDefault()
-        this.props.nextStep()
+        console.log(this.props.value.email)
+        if (emailRegex.test(this.props.value.email)) {
+            this.setState({ wrongEmail: false })
+            this.props.nextStep()
+        }
+        else
+            this.setState({ wrongEmail: true })
     }
+
     render() {
         const { handleChange, value } = this.props
         return (
             <div>
-                <a>Necesitamos los siguientes datos para poder encontrar sus datos</a>
+                <a>Necesitamos la siguietne informacion para poder encontrar sus datos</a>
                 <br />
-                <IntlTelInput
-                    preferredCountries={['ar']}
-                    separateDialCode={true}
-                    type="number"
-                    name="phone"
-                    onChange={handleChange('phone')}
-                    value={value.phone}
-                    required
-                />
                 <Form.Group>
                     <Form.Label>Mail</Form.Label>
                     <Form.Control
-                        type="text"
+                        type="email"
                         name='email'
                         onChange={handleChange('email')}
                         value={value.email}
                         required
                     />
+                    {this.state.wrongEmail === true ?
+                        (<Form.Text className="text-muted">
+                            Debe introducir su email.
+                        </Form.Text>) : (null)
+                    }
+                    <br />
+                    <Button
+                        className="backButton"
+                        variant="contained"
+                        color="secondary"
+                        onClick={this.back}
+                    > Atras</Button>
+                    <Button
+                        className="forwardButton"
+                        onClick={this.continue}
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                    >Continue</Button>
                 </Form.Group>
-                <Button
-                    type="submit"
-                    className="backButton"
-                    variant="contained"
-                    color="secondary"
-                    onClick={this.back}
-                > Atras</Button>
-                <Button
-                    className="forwardButton"
-                    onClick={this.continue}
-                    variant="contained"
-                    color="primary"
-                    type="submit"
-                >Continue</Button>
             </div>
         );
     }
