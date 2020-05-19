dc7a0ab9f936a3d42927da131c0d8d70264ca413
MilenaL2018 <milelucero_09@hotmail.com>
2020-05-19 13:26:44 -0300

    Looking for alternatives to the form style

--- src/Global/Main.jsx
+++ src/Global/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Form from "./Formcopy";
+import FormMile from "./formMile";
 
 
 const Main = () => (
@@ -9,8 +10,7 @@ const Main = () => (
         <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/form" component={Form} />
-            
-
+            <Route path="/prueba" component={FormMile} />
         </Switch>
     </div>
 );
