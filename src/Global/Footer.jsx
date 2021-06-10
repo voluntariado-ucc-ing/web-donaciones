4fcbaf8b6b21720203ccbc3ba331bfad93144b10
cata99 <48757813+cata99@users.noreply.github.com>
2021-06-10 19:16:30 -0300

    footer listo

--- src/Global/Footer.jsx
+++ src/Global/Footer.jsx
@@ -6,8 +6,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelopeOpen, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import Col from "react-bootstrap/Col";
+import oficial from "../images/oficial.png";
+import logovol from "../images/logo.png";
+import correo from "../images/correo.png";
+import telefono from "../images/telefono.png";
 
 import "./css/Footer.css"
+import { RoomTwoTone } from "@material-ui/icons";
 
 
 class Footer extends Component {
@@ -15,90 +20,94 @@ class Footer extends Component {
         return (
             <div>
                 <Container fluid className={"site-section heading site-color how computer-footer"}>
-                    <Row className={"justify-text-center"}>
-                        <p>Voluntariado Barrio El Milagro</p>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <FontAwesomeIcon icon={faEnvelopeOpen} />
-                            <a
-                                target="_top"
-                                rel="noopener noreferrer"
-                                href="mailto:voluntariado.vmru@ucc.edu.ar">
-                                <p className={"text-light"}>voluntariado.vmru@ucc.edu.ar</p>
-                            </a>
-                        </Col>
-                        <Col>
-                            <FontAwesomeIcon icon={faInstagram} />
-                            <a target="_blank" href="https://www.instagram.com/voluntariadouccing/" rel="noopener noreferrer">
-                                <p className={"text-light"}>
-                                    @voluntariadouccing
-                                </p>
-                            </a>
-                            <a target="_blank" href={"/volunteers"} rel="noopener noreferrer">
-                                    <p className={"text-light"}>
-                                        Excel voluntarios
-                                    </p>
-                                </a>
-                        </Col>
-                        <Col>
-                            <FontAwesomeIcon icon={faPhone} />
-                            <a href="tel:+5493514938000,510"
-                                target="_top"
-                                rel="noopener noreferrer">
-                                <p className={"text-light"}>
-                                    (0351) 493 8000 int. 510
-                                    </p>
-                            </a>
-                        </Col>
-                    </Row>
-                </Container>
+                    <div class="Title">
+                        <Row>
+                            <h5>Voluntariado Barrio El Milagro</h5>
+                        </Row>
+                    </div>
+                    <div>
+                        <Row>
 
-                <Container fluid className={"site-section heading site-color how mobile-footer"}>
-                    <Col className={"justify-text-center"}>
-                        <Row>
-                            <p>Voluntariado Barrio El Milagro</p>
-                        </Row>
-                        <Row>
-                            <Col sm>
-                                <FontAwesomeIcon icon={faInstagram} />
+                            <Col>
+                                <h6>Voluntariado</h6>
                             </Col>
                             <Col>
-                                <a target="_blank" href="https://www.instagram.com/voluntariadouccing/" rel="noopener noreferrer">
-                                    <p className={"text-light"}>
-                                        @voluntariadouccing
-                                    </p>
-                                </a>
+                                <h6>Universidad Católica de Córdoba</h6>
                             </Col>
                         </Row>
                         <Row>
-                            <Col sm>
-                                <FontAwesomeIcon icon={faEnvelopeOpen} />
+                            <Col>
+                                <a href=" https://www.instagram.com/voluntariadouccing" alt="Instagram" title="Instagram" rel="noreferrer" target="_blank">
+                                    <img src="https://uccor.edu.ar/portal2015/UniversidadCatolica/images/instagram.png" class="boing" alt="Instagram"></img></a>
+                                <a href="mailto:voluntariado.vmru@ucc.edu.ar">
+                                    <img
+                                        alt="ucc"
+                                        src={correo}
+                                        width="50"
+                                        height="50"
+                                        className="d-inline-block align-top"
+
+                                    />
+                                </a>
+                                <a href="https://youtu.be/soUWTT424Z4" alt="Youtube" title="Youtube" rel="noreferrer" target="_blank">
+                                    <img src="https://uccor.edu.ar/portal2015/UniversidadCatolica/images/youtube.png" class="boing" alt="Youtube"></img>
+                                </a>
+                                <a href="tel:+5493514938000,510" alt="Telefono" title="Telefono" rel="noreferrer" target="_blank">
+                                    <img
+                                        alt="ucc"
+                                        src={telefono}
+                                        width="50"
+                                        height="50"
+                                        className="d-inline-block align-top"
+
+                                    /></a>
                             </Col>
                             <Col>
-                                <a
-                                    target="_top"
-                                    rel="noopener noreferrer"
-                                    href="mailto:voluntariado.vmru@ucc.edu.ar">
-                                    <p className={"text-light"}>voluntariado.vmru@ucc.edu.ar</p>
+                                <a href="https://www.instagram.com/uccoficial" alt="Instagram" title="Instagram" rel="noreferrer" target="_blank">
+                                    <img src="https://uccor.edu.ar/portal2015/UniversidadCatolica/images/instagram.png" class="boing" alt="Instagram"></img>
+                                </a>
+                                <a href="https://www.twitter.com/uccoficial" alt="Twitter" title="Twitter" rel="noreferrer" target="_blank">
+                                    <img src="https://uccor.edu.ar/portal2015/UniversidadCatolica/images/twitter.png" class="boing" alt="Twitter"></img>
+                                </a>
+                                <a href="https://www.youtube.com/uccoficial" alt="Youtube" title="Youtube" rel="noreferrer" target="_blank">
+                                    <img src="https://uccor.edu.ar/portal2015/UniversidadCatolica/images/youtube.png" class="boing" alt="Youtube"></img>
+                                </a>
+                                <a href="https://www.facebook.com/UniversidadCatolicaCordoba" alt="Facebook" title="Facebook" rel="noreferrer" target="_blank">
+                                    <img src="https://uccor.edu.ar/portal2015/UniversidadCatolica/images/fbk.png" class="boing" alt="Facebook"></img>
                                 </a>
                             </Col>
                         </Row>
-                        <Row >
-                            <Col sm>
-                                <FontAwesomeIcon icon={faPhone} />
-                            </Col>
-                            <Col sm>
-                                <a href="tel:+5493514938000,510"
-                                    target="_top"
-                                    rel="noopener noreferrer">
-                                    <p className={"text-light"}>
-                                        (0351) 493 8000 int. 510
-                                    </p>
-                                </a>
-                            </Col>
-                        </Row>
-                    </Col>
+                    </div>
+                    <div class="Pictures">
+                        <a>
+                            <img
+
+                                alt="ucc"
+                                src={logovol}
+                                width="50"
+                                height="50"
+                                className="d-inline-block align-top"
+
+                            />
+                        </a>
+
+                        <a href="https://ucc.edu.ar">
+                            <img
+
+                                alt="ucc"
+                                src={oficial}
+                                width="90"
+                                height="50"
+                                className="d-inline-block align-top"
+
+                            /></a>
+
+                    </div>
+                       <a target="_blank" href={"/volunteers"} rel="noopener noreferrer">
+                            <p className={"text-light"}>
+                                Excel voluntarios
+                            </p>
+                        </a>
                 </Container>
             </div >
         )
